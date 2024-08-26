056f57bc388057d2ead0e6edc06f76010b571eca
Lucas-AVS <lucas.alencar31@gmail.com>
2024-08-26 17:31:12 -0300

    feat: add subtract, divide, and multiply functions to calculator, tested with Jest

--- calculator.js
+++ calculator.js
@@ -9,16 +9,34 @@ class Calculator {
     }
   }
 
-  subtract() {
-    return this.n1 - this.n2;
+  subtract(n1, n2) {
+    typeof n1 === "number" ? (n1 = n1) : (n1 = parseInt(n1));
+    typeof n2 === "number" ? (n2 = n2) : (n2 = parseInt(n2));
+    if (isNaN(n1) || isNaN(n2)) {
+      return "ERROR: NaN";
+    } else {
+      return n1 - n2;
+    }
   }
 
-  divide() {
-    return this.n1 / this.n2;
+  divide(n1, n2) {
+    typeof n1 === "number" ? (n1 = n1) : (n1 = parseInt(n1));
+    typeof n2 === "number" ? (n2 = n2) : (n2 = parseInt(n2));
+    if (isNaN(n1) || isNaN(n2)) {
+      return "ERROR: NaN";
+    } else {
+      return n1 / n2;
+    }
   }
 
-  multiply() {
-    return this.n1 * this.n2;
+  multiply(n1, n2) {
+    typeof n1 === "number" ? (n1 = n1) : (n1 = parseInt(n1));
+    typeof n2 === "number" ? (n2 = n2) : (n2 = parseInt(n2));
+    if (isNaN(n1) || isNaN(n2)) {
+      return "ERROR: NaN";
+    } else {
+      return n1 * n2;
+    }
   }
 }
 
