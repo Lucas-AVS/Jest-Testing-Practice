940bcf49fb28e352a466d8ec1d08fa1258302ad0
Lucas-AVS <lucas.alencar31@gmail.com>
2024-08-26 18:24:16 -0300

    test: update test to use 'toBe' for direct object comparison

--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -1,7 +1,7 @@
 const AnalyzeArray = require("./lib/analyzeArray");
 
 test(" takes an array of numbers and returns an object with the following properties: average, min, max, and length", () => {
-  expect(new AnalyzeArray([1, 8, 3, 4, 2, 6])).toBe({
+  expect(new AnalyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
     average: 4,
     min: 1,
     max: 8,
